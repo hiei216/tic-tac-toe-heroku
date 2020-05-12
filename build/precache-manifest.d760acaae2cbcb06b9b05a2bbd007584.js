8de158a4d23cc29fbcb44715e4264b865b7fbe29
Jirko <hiei216@gmail.com>
2020-05-13 00:33:44 +0200

    Add files via upload

---
+++ build/precache-manifest.d760acaae2cbcb06b9b05a2bbd007584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79f12b9b6245cc365e844fdec3d6dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a822b6d335a5945f25d4",
+    "url": "/static/css/main.3d2915ae.chunk.css"
+  },
+  {
+    "revision": "3d7c31b332aef51780e5",
+    "url": "/static/js/2.02902e94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02902e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a822b6d335a5945f25d4",
+    "url": "/static/js/main.b19f8c5d.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
